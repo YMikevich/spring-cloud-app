754e03b77bf8dca8f4418543ee8b2f4cadaf5dcb
YMikevich <z.mikevich@icloud.com>
2019-09-17 10:02:57 +0300

    Revert "Revert "#4 checkstyle maven plugin added and configured, code refactoring based on checkstyle violations completed""
    This reverts commit a9db9d89

--- clients/twitter-integration/src/main/java/com/github/ymikevich/twitter/integration/services/Twitter4jSearchEngine.java
+++ clients/twitter-integration/src/main/java/com/github/ymikevich/twitter/integration/services/Twitter4jSearchEngine.java
@@ -2,10 +2,15 @@ package com.github.ymikevich.twitter.integration.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import twitter4j.*;
+import twitter4j.Status;
+import twitter4j.Twitter;
 
 import java.util.List;
 
+/**
+ * The type Twitter4j search engine.
+ * TweetSearchEngine implementation.
+ */
 @Service
 @RequiredArgsConstructor
 public class Twitter4jSearchEngine implements TweetSearchEngine {
