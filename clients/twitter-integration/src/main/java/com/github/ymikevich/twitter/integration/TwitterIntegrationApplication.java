a5ec05efc97f7d33d62b845fd078970dc3cf5f92
YMikevich <50046389+YMikevich@users.noreply.github.com>
2019-09-17 15:24:30 +0300

    Feature/4/add code quality tools (#10)
    * #4 checkstyle maven plugin added and configured, code refactoring based on checkstyle violations completed
    
    * #4 checkstyle and PMD maven plugins added and configured, code refactoring based on checkstyle violations completed
    
    * Revert "#4 checkstyle maven plugin added and configured, code refactoring based on checkstyle violations completed"
    
    This reverts commit 596fa482
    
    * Revert "Revert "#4 checkstyle maven plugin added and configured, code refactoring based on checkstyle violations completed""
    
    This reverts commit a9db9d89

--- clients/twitter-integration/src/main/java/com/github/ymikevich/twitter/integration/TwitterIntegrationApplication.java
+++ clients/twitter-integration/src/main/java/com/github/ymikevich/twitter/integration/TwitterIntegrationApplication.java
@@ -3,8 +3,18 @@ package com.github.ymikevich.twitter.integration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * This class loads Spring Boot.
+ */
 @SpringBootApplication
 public class TwitterIntegrationApplication {
 
-    public static void main(String[] args) { SpringApplication.run(TwitterIntegrationApplication.class, args); }
+    /**
+     * Main.
+     *
+     * @param args the arguments
+     */
+    public static void main(final String[] args) {
+        SpringApplication.run(TwitterIntegrationApplication.class, args);
+    }
 }
--- clients/twitter-integration/src/main/java/com/github/ymikevich/twitter/integration/services/TweetSearchEngine.java
+++ clients/twitter-integration/src/main/java/com/github/ymikevich/twitter/integration/services/TweetSearchEngine.java
@@ -4,7 +4,16 @@ import twitter4j.Status;
 
 import java.util.List;
 
+/**
+ * The interface Tweet search engine.
+ */
 public interface TweetSearchEngine {
 
+    /**
+     * Find tweets by username list.
+     *
+     * @param username the username
+     * @return the list of tweets
+     */
     List<Status> findTweetsByUsername(String username);
 }
