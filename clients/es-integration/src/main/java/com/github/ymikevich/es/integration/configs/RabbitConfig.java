8d6c33ef289417c01b8e70c6d75c9f4146268d59
YMikevich <z.mikevich@icloud.com>
2019-09-25 16:09:54 +0300

    #11 test implementation of rabbitmq receiver added

---
+++ clients/es-integration/src/main/java/com/github/ymikevich/es/integration/configs/RabbitConfig.java
@@ -0,0 +1,42 @@
+package com.github.ymikevich.es.integration.configs;
+
+import com.github.ymikevich.es.integration.domain.Receiver;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+
+@Configuration
+public class RabbitConfig {
+
+    private final String queueName;
+
+    public RabbitConfig(@Value("${rabbit.queue.name}") final String queueName) {
+        this.queueName = queueName;
+    }
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+                                             MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(queueName);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
+}
