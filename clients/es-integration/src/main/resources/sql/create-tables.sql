358e317f1e66c42f20a37e81b02744fec757ec95
YMikevich <z.mikevich@icloud.com>
2019-10-21 16:24:12 +0300

    #17 sql scripts refactoring and reformatting, triggers and the view added

--- clients/es-integration/src/main/resources/sql/create-tables.sql
+++ clients/es-integration/src/main/resources/sql/create-tables.sql
@@ -1,97 +1,163 @@
-CREATE TABLE app_user(
-	id SERIAL PRIMARY KEY,
-	role_id INTEGER NOT NULL,
-	country_id INTEGER,
-	postal_code_id INTEGER,
-	hobby_id INTEGER,
-	image_id INTEGER,
-	name VARCHAR (50) UNIQUE NOT NULL,
-	email VARCHAR (100) UNIQUE NOT NULL,
-	created_at TIMESTAMP NOT NULL,
-	modified_at TIMESTAMP NOT NULL,
-	partner INTEGER,
-	CONSTRAINT role_role_id_fkey FOREIGN KEY (role_id)
-	REFERENCES role (id) MATCH SIMPLE,
-	CONSTRAINT country_country_id_fkey FOREIGN KEY (country_id)
-	REFERENCES country (id) MATCH SIMPLE,
-	CONSTRAINT postal_code_postal_code_id_fkey FOREIGN KEY (postal_code_id)
-	REFERENCES postal_code (id) MATCH SIMPLE,
-	CONSTRAINT hobby_hobby_id_fkey FOREIGN KEY (hobby_id)
-	REFERENCES hobby (id) MATCH SIMPLE,
-	CONSTRAINT image_image_id_fkey FOREIGN KEY (image_id)
-	REFERENCES image (id) MATCH SIMPLE
+CREATE TABLE role
+(
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL
 );
 
-CREATE TABLE role(
-	id SERIAL PRIMARY KEY,
-	name VARCHAR (50) UNIQUE NOT NULL
+CREATE TABLE country
+(
+    id       SERIAL PRIMARY KEY,
+    name     VARCHAR(50) UNIQUE NOT NULL,
+    iso_code VARCHAR(13) UNIQUE NOT NULL
 );
 
-CREATE TABLE account(
-	id SERIAL PRIMARY KEY,
-	nickname VARCHAR (50) UNIQUE NOT NULL,
-	email VARCHAR (50) UNIQUE
+CREATE TABLE postal_code
+(
+    id   SERIAL PRIMARY KEY,
+    code VARCHAR(10) UNIQUE NOT NULL
 );
 
-CREATE TABLE app_user_account(
-	app_user_id INTEGER NOT NULL,
-	account_id INTEGER NOT NULL,
-	PRIMARY KEY (app_user_id, account_id),
-	CONSTRAINT app_user_account_account_id_fkey FOREIGN KEY (account_id)
-	REFERENCES account (id) MATCH SIMPLE,
-	CONSTRAINT app_user_app_user_id_fkey FOREIGN KEY (app_user_id)
-	REFERENCES app_user (id) MATCH SIMPLE
+CREATE TABLE hobby
+(
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR(50) NOT NULL,
+    description VARCHAR(500)
 );
 
-CREATE TABLE country(
-	id SERIAL PRIMARY KEY,
-	name VARCHAR (50) UNIQUE NOT NULL,
-	iso_code VARCHAR (13) UNIQUE NOT NULL
+CREATE TABLE image
+(
+    id        SERIAL PRIMARY KEY,
+    file_type VARCHAR(10) NOT NULL,
+    image     BYTEA       NOT NULL
 );
 
-CREATE TABLE postal_code(
-	id SERIAL PRIMARY KEY,
-	code VARCHAR (10) UNIQUE NOT NULL
+CREATE TABLE app_user
+(
+    id             SERIAL PRIMARY KEY,
+    role_id        INTEGER             NOT NULL,
+    country_id     INTEGER,
+    postal_code_id INTEGER,
+    hobby_id       INTEGER,
+    image_id       INTEGER,
+    name           VARCHAR(50) UNIQUE  NOT NULL,
+    email          VARCHAR(100) UNIQUE NOT NULL,
+    created_at     TIMESTAMP           NOT NULL,
+    modified_at    TIMESTAMP           NOT NULL,
+    partner_id     INTEGER,
+    gender         VARCHAR(1)          NOT NULL,
+    CONSTRAINT role_role_id_fkey FOREIGN KEY (role_id)
+        REFERENCES role (id) MATCH SIMPLE,
+    CONSTRAINT country_country_id_fkey FOREIGN KEY (country_id)
+        REFERENCES country (id) MATCH SIMPLE,
+    CONSTRAINT postal_code_postal_code_id_fkey FOREIGN KEY (postal_code_id)
+        REFERENCES postal_code (id) MATCH SIMPLE,
+    CONSTRAINT hobby_hobby_id_fkey FOREIGN KEY (hobby_id)
+        REFERENCES hobby (id) MATCH SIMPLE,
+    CONSTRAINT image_image_id_fkey FOREIGN KEY (image_id)
+        REFERENCES image (id) MATCH SIMPLE
 );
 
-CREATE TABLE image(
-	id SERIAL PRIMARY KEY,
-	file_type VARCHAR(10) NOT NULL,
-	image BYTEA NOT NULL
+CREATE TABLE account
+(
+    id       SERIAL PRIMARY KEY,
+    nickname VARCHAR(50) UNIQUE NOT NULL,
+    email    VARCHAR(50) UNIQUE
 );
 
-CREATE TABLE passport(
-	id SERIAL PRIMARY KEY,
-	app_user_id INTEGER NOT NULL,
-	image_id INTEGER NOT NULL,
-	country_id INTEGER NOT NULL,
-	CONSTRAINT image_image_id_fkey FOREIGN KEY (image_id)
-	REFERENCES image (id) MATCH SIMPLE,
-	CONSTRAINT country_country_id_fkey FOREIGN KEY (country_id)
-	REFERENCES country (id) MATCH SIMPLE,
-	CONSTRAINT app_user_app_user_id_fkey FOREIGN KEY (app_user_id)
-	REFERENCES app_user (id) MATCH SIMPLE
+CREATE TABLE user_account
+(
+    user_id    INTEGER NOT NULL,
+    account_id INTEGER NOT NULL,
+    PRIMARY KEY (user_id, account_id),
+    CONSTRAINT user_account_account_id_fkey FOREIGN KEY (account_id)
+        REFERENCES account (id) MATCH SIMPLE,
+    CONSTRAINT user_user_id_fkey FOREIGN KEY (user_id)
+        REFERENCES app_user (id) MATCH SIMPLE
 );
 
-CREATE TABLE visa(
-    id SERIAL PRIMARY KEY,
+CREATE TABLE passport
+(
+    id         SERIAL PRIMARY KEY,
+    user_id    INTEGER     NOT NULL,
+    image_id   INTEGER,
+    country_id INTEGER     NOT NULL,
+    number     VARCHAR(20) NOT NULL,
+    CONSTRAINT image_image_id_fkey FOREIGN KEY (image_id)
+        REFERENCES image (id) MATCH SIMPLE,
+    CONSTRAINT country_country_id_fkey FOREIGN KEY (country_id)
+        REFERENCES country (id) MATCH SIMPLE,
+    CONSTRAINT user_user_id_fkey FOREIGN KEY (user_id)
+        REFERENCES app_user (id) MATCH SIMPLE
+);
+
+CREATE TABLE visa
+(
+    id          SERIAL PRIMARY KEY,
     passport_id INTEGER NOT NULL,
-    country_id INTEGER NOT NULL,
-    type VARCHAR(10),
-	CONSTRAINT passport_passport_id_fkey FOREIGN KEY (passport_id)
-	REFERENCES passport (id) MATCH SIMPLE,
-	CONSTRAINT country_country_id_fkey FOREIGN KEY (country_id)
-	REFERENCES country (id) MATCH SIMPLE
+    country_id  INTEGER NOT NULL,
+    type        VARCHAR(10),
+    CONSTRAINT passport_passport_id_fkey FOREIGN KEY (passport_id)
+        REFERENCES passport (id) MATCH SIMPLE,
+    CONSTRAINT country_country_id_fkey FOREIGN KEY (country_id)
+        REFERENCES country (id) MATCH SIMPLE
 );
 
-CREATE TABLE hobby(
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(10) NOT NULL,
-	description VARCHAR(500)
-);
-
-CREATE FUNCTION countUsersInCountry(IN country VARCHAR, OUT total INTEGER) RETURNS INTEGER AS $$
+CREATE OR REPLACE FUNCTION count_users_in_country(IN country_name VARCHAR, OUT total INTEGER) RETURNS INTEGER AS
+$$
 BEGIN
-SELECT COUNT(*) INTO total FROM app_user JOIN country ON app_user.country_id=country.id WHERE country.name=country;
-END; $$
-LANGUAGE PLPGSQL;
+    SELECT COUNT(*)
+    INTO total
+    FROM app_user
+             JOIN country ON app_user.country_id = country.id
+    WHERE country.name = country_name;
+END;
+$$
+    LANGUAGE PLPGSQL;
+
+CREATE VIEW illegal_bisexuals_in_usa AS
+SELECT app_user.name
+FROM app_user
+         LEFT JOIN passport ON passport.user_id = app_user.id
+         LEFT JOIN visa ON visa.id = passport.id
+         LEFT JOIN role ON role.id = app_user.role_id
+         LEFT JOIN app_user AS user_partner
+                   ON app_user.id = user_partner.partner_id
+WHERE (passport.user_id IS NULL
+    OR (passport.user_id IS NOT NULL AND visa.id IS NULL AND visa.country_id != app_user.country_id))
+  AND app_user.gender = user_partner.gender;
+
+CREATE FUNCTION name_changed() RETURNS TRIGGER
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    IF NEW.name != OLD.name THEN
+        NEW.modified_at := current_timestamp;
+    END IF;
+    RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER trigger_name_changed
+    BEFORE UPDATE
+    ON app_user
+    FOR EACH ROW
+EXECUTE PROCEDURE name_changed();
+
+CREATE FUNCTION email_changed() RETURNS TRIGGER
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    IF NEW.email != OLD.email THEN
+        NEW.modified_at := current_timestamp;
+    END IF;
+    RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER trigger_email_changed
+    BEFORE UPDATE
+    ON app_user
+    FOR EACH ROW
+EXECUTE PROCEDURE email_changed();
--- clients/es-integration/src/main/resources/sql/populate-db.sql
+++ clients/es-integration/src/main/resources/sql/populate-db.sql
@@ -1,46 +1,88 @@
-INSERT INTO hobby (name, description) VALUES ('music', 'playing guitar');
-INSERT INTO hobby (name, description) VALUES ('music', 'playing piano');
-INSERT INTO hobby (name, description) VALUES ('travelling', 'visiting different countries');
-INSERT INTO hobby (name, description) VALUES ('streetracing', 'night races on sport cars');
-INSERT INTO hobby (name, description) VALUES ('memes', 'observing all kind of memes');
+INSERT INTO hobby (id, name, description)
+VALUES (1, 'music', 'playing guitar');
+INSERT INTO hobby (id, name, description)
+VALUES (2, 'music', 'playing piano');
+INSERT INTO hobby (id, name, description)
+VALUES (3, 'travelling', 'visiting different countries');
+INSERT INTO hobby (id, name, description)
+VALUES (4, 'streetracing', 'night races on sport cars');
+INSERT INTO hobby (id, name, description)
+VALUES (5, 'memes', 'observing all kind of memes');
 
-INSERT INTO role (name) VALUES ('moderator');
-INSERT INTO role (name) VALUES ('reader');
-INSERT INTO role (name) VALUES ('loh');
+INSERT INTO role (id, name)
+VALUES (1, 'moderator');
+INSERT INTO role (id, name)
+VALUES (2, 'reader');
+INSERT INTO role (id, name)
+VALUES (3, 'loh');
 
-INSERT INTO postal_code (code) VALUES ('228322');
-INSERT INTO postal_code (code) VALUES ('230023');
-INSERT INTO postal_code (code) VALUES ('278322');
-INSERT INTO postal_code (code) VALUES ('228324');
-INSERT INTO postal_code (code) VALUES ('217322');
+INSERT INTO postal_code (id, code)
+VALUES (1, '228322');
+INSERT INTO postal_code (id, code)
+VALUES (2, '230023');
+INSERT INTO postal_code (id, code)
+VALUES (3, '278322');
+INSERT INTO postal_code (id, code)
+VALUES (4, '228324');
+INSERT INTO postal_code (id, code)
+VALUES (5, '217322');
 
-INSERT INTO account (nickname, email) VALUES ('PDChina', 'pdchina@gmail.com');
-INSERT INTO account (nickname, email) VALUES ('sonicdeath', 'sonicdeath@gmail.com');
-INSERT INTO account (nickname, email) VALUES ('humortranslate', 'humortranslate@gmail.com');
-INSERT INTO account (nickname, email) VALUES ('anger6subu', 'anger6subu@gmail.com');
+INSERT INTO account (id, nickname, email)
+VALUES (1, 'PDChina', 'pdchina@gmail.com');
+INSERT INTO account (id, nickname, email)
+VALUES (2, 'sonicdeath', 'sonicdeath@gmail.com');
+INSERT INTO account (id, nickname, email)
+VALUES (3, 'humortranslate', 'humortranslate@gmail.com');
+INSERT INTO account (id, nickname, email)
+VALUES (4, 'anger6subu', 'anger6subu@gmail.com');
 
-INSERT INTO country (name, iso_code) VALUES ('Belarus', 'ISO 3166-2:BY');
-INSERT INTO country (name, iso_code) VALUES ('Russia', 'ISO 3166-2:RU');
-INSERT INTO country (name, iso_code) VALUES ('Germany', 'ISO 3166-2:DE');
-INSERT INTO country (name, iso_code) VALUES ('China', 'ISO 3166-2:CN');
-INSERT INTO country (name, iso_code) VALUES ('USA', 'ISO 3166-2:US');
+INSERT INTO country (id, name, iso_code)
+VALUES (1, 'Belarus', 'ISO 3166-2:BY');
+INSERT INTO country (id, name, iso_code)
+VALUES (2, 'Russia', 'ISO 3166-2:RU');
+INSERT INTO country (id, name, iso_code)
+VALUES (3, 'Germany', 'ISO 3166-2:DE');
+INSERT INTO country (id, name, iso_code)
+VALUES (4, 'China', 'ISO 3166-2:CN');
+INSERT INTO country (id, name, iso_code)
+VALUES (5, 'USA', 'ISO 3166-2:US');
 
-INSERT INTO app_user (role_id, country_id, postal_code_id, hobby_id, name, email, partner, gender, created_at, modified_at) VALUES (1, 1, 2, 8, 'Sanya', 'sanya_boss@mail.ru', 2, 'M', current_timestamp, current_timestamp);
-INSERT INTO app_user (role_id, country_id, postal_code_id, hobby_id, name, email, partner, gender, created_at, modified_at) VALUES (2, 1, 2, 6, 'Valia', 'valia2002@mail.ru', 1, 'W', current_timestamp, current_timestamp);
-INSERT INTO app_user (role_id, country_id, postal_code_id, hobby_id, name, email, partner, gender, created_at, modified_at) VALUES (1, 4, 4, 7, 'Konfucyi', 'hello_world@yandex.ru', 4, 'M', current_timestamp, current_timestamp);
-INSERT INTO app_user (role_id, country_id, postal_code_id, hobby_id, name, email, partner, gender, created_at, modified_at) VALUES (3, 5, 3, 6, 'Subu Rakapatah', 'subu@teachpoint.com', 3, 'M', current_timestamp, current_timestamp);
+INSERT INTO app_user (id, role_id, country_id, postal_code_id, hobby_id, name, email, partner, gender, created_at,
+                      modified_at)
+VALUES (1, 1, 1, 2, 4, 'Sanya', 'sanya_boss@mail.ru', 2, 'M', current_timestamp, current_timestamp);
+INSERT INTO app_user (id, role_id, country_id, postal_code_id, hobby_id, name, email, partner, gender, created_at,
+                      modified_at)
+VALUES (2, 2, 1, 2, 2, 'Valia', 'valia2002@mail.ru', 1, 'W', current_timestamp, current_timestamp);
+INSERT INTO app_user (id, role_id, country_id, postal_code_id, hobby_id, name, email, partner, gender, created_at,
+                      modified_at)
+VALUES (3, 1, 4, 4, 3, 'Konfucyi', 'hello_world@yandex.ru', 4, 'M', current_timestamp, current_timestamp);
+INSERT INTO app_user (id, role_id, country_id, postal_code_id, hobby_id, name, email, partner, gender, created_at,
+                      modified_at)
+VALUES (4, 2, 5, 3, 2, 'Subu Rakapatah', 'subu@teachpoint.com', 3, 'M', current_timestamp, current_timestamp);
 
-INSERT INTO user_account (user_id, account_id) VALUES (2, 3);
-INSERT INTO user_account (user_id, account_id) VALUES (2, 2);
-INSERT INTO user_account (user_id, account_id) VALUES (4, 1);
-INSERT INTO user_account (user_id, account_id) VALUES (5, 4);
+INSERT INTO user_account (user_id, account_id)
+VALUES (1, 3);
+INSERT INTO user_account (user_id, account_id)
+VALUES (1, 2);
+INSERT INTO user_account (user_id, account_id)
+VALUES (3, 1);
+INSERT INTO user_account (user_id, account_id)
+VALUES (4, 4);
 
-INSERT INTO passport (user_id, country_id, number) VALUES (2, 1, 'KH12345678');
-INSERT INTO passport (user_id, country_id, number) VALUES (3, 1, 'KH87654321');
-INSERT INTO passport (user_id, country_id, number) VALUES (4, 4, 'DS4F44S2DD');
+INSERT INTO passport (id, user_id, country_id, number)
+VALUES (1, 1, 1, 'KH12345678');
+INSERT INTO passport (id, user_id, country_id, number)
+VALUES (2, 2, 1, 'KH87654321');
+INSERT INTO passport (id, user_id, country_id, number)
+VALUES (3, 3, 4, 'DS4F44S2DD');
 
-INSERT INTO visa (passport_id, country_id, type) VALUES (1, 1, 'National');
-INSERT INTO visa (passport_id, country_id, type) VALUES (1, 3, 'Touristic');
-INSERT INTO visa (passport_id, country_id, type) VALUES (2, 1, 'Touristic');
-INSERT INTO visa (passport_id, country_id, type) VALUES (3, 4, 'National');
-INSERT INTO visa (passport_id, country_id, type) VALUES (3, 5, 'Touristic');
+INSERT INTO visa (id, passport_id, country_id, type)
+VALUES (1, 1, 1, 'National');
+INSERT INTO visa (id, passport_id, country_id, type)
+VALUES (2, 1, 3, 'Touristic');
+INSERT INTO visa (id, passport_id, country_id, type)
+VALUES (3, 2, 1, 'Touristic');
+INSERT INTO visa (id, passport_id, country_id, type)
+VALUES (4, 3, 4, 'National');
+INSERT INTO visa (id, passport_id, country_id, type)
+VALUES (5, 3, 5, 'Touristic');
